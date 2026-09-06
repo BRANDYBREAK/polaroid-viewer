0b322c5c3cd023ec46466227bb4e929a9980745d
BRANDYBREAK <gevelajohnlexter@gmail.com>
2026-09-06 10:52:14 +0800

    Update photos.js

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,23 +1,23 @@
 export const photos = [
     {
         id: 1,
-        src: "/photos/SweetLittleGlance.webp",
-        caption: "Sweet Little Glance",
+        src: "/photos/P1.jpg",
+        caption: "SAfterglow With You",
         date: "September 6, 2026",
         rotation: "-rotate-2"
     },
     {
         id: 2,
-        src: "/photos/AfterglowWithYou.webp",
+        src: "/photos/P2.jpg",
         caption: "Afterglow With You",
         date: "September 7, 2026",
-        rotation: "rotate-1"
+        rotation: "rotate-2"
     },
     {
         id: 3,
-        src: "/photos/FlowersFoundHer.webp",
-        caption: "Flowers Found Her",
+        src: "/photos/P3.jpg",
+        caption: "Afterglow With You",
         date: "September 8, 2026",
-        rotation: "-rotate-1"
+        rotation: "-rotate-2"
     }
 ];
