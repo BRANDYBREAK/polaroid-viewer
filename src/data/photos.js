a3bf2b8bc2f76784fcbf0c27cc95a252ad94121a
BRANDYBREAK <gevelajohnlexter@gmail.com>
2026-09-06 10:59:13 +0800

    Refactor photo source paths to use URL constructor
    Updated photo source paths to use URL constructor for module compatibility.

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,23 +1,23 @@
 export const photos = [
-    {
-        id: 1,
-        src: "../assets/photos/P1.jpg",
-        caption: "Afterglow With You",
-        date: "September 6, 2026",
-        rotation: "-rotate-2"
-    },
-    {
-        id: 2,
-        src: "../assets/photos/P2.jpg",
-        caption: "Afterglow With You",
-        date: "September 7, 2026",
-        rotation: "rotate-2"
-    },
-    {
-        id: 3,
-        src: "../assets/photos/P3.jpg",
-        caption: "Afterglow With You",
-        date: "September 8, 2026",
-        rotation: "-rotate-2"
-    }
+  {
+    id: 1,
+    src: new URL("../assets/photos/P1.jpg", import.meta.url).href,
+    caption: "Afterglow With You",
+    date: "September 6, 2026",
+    rotation: "-rotate-2",
+  },
+  {
+    id: 2,
+    src: new URL("../assets/photos/P2.jpg", import.meta.url).href,
+    caption: "Afterglow With You",
+    date: "September 7, 2026",
+    rotation: "rotate-2",
+  },
+  {
+    id: 3,
+    src: new URL("../assets/photos/P3.jpg", import.meta.url).href,
+    caption: "Afterglow With You",
+    date: "September 8, 2026",
+    rotation: "-rotate-2",
+  },
 ];
