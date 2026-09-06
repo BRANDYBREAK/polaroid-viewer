84345e7613e644fcd569c30b5795dadba67a53b8
BRANDYBREAK <gevelajohnlexter@gmail.com>
2026-09-06 10:57:30 +0800

    Update photo source paths to use relative URLs

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,21 +1,21 @@
 export const photos = [
     {
         id: 1,
-        src: "/photos/P1.jpg",
+        src: "../assets/photos/P1.jpg",
         caption: "Afterglow With You",
         date: "September 6, 2026",
         rotation: "-rotate-2"
     },
     {
         id: 2,
-        src: "/photos/P2.jpg",
+        src: "../assets/photos/P2.jpg",
         caption: "Afterglow With You",
         date: "September 7, 2026",
         rotation: "rotate-2"
     },
     {
         id: 3,
-        src: "/photos/P3.jpg",
+        src: "../assets/photos/P3.jpg",
         caption: "Afterglow With You",
         date: "September 8, 2026",
         rotation: "-rotate-2"
